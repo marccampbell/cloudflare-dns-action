577f58b856c9c10ae11e1972740ead245ee4d12b
Marc Campbell <marc.e.campbell@gmail.com>
2024-01-17 18:15:51 -0700

    get current record id

--- create-dns-record/src/cloudflare.ts
+++ create-dns-record/src/cloudflare.ts
@@ -1,4 +1,24 @@
+import * as httpClient from '@actions/http-client';
 
-export async function getCurrentRecordId(token: string, name: string): Promise<string|undefined> {
-  return undefined;
+export async function getCurrentRecordId(token: string, zoneId: string, name: string): Promise<string|undefined> {
+  const http = new httpClient.HttpClient()
+
+  const headers = {
+    "Authorization": `Bearer ${token}`,
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  };
+
+  const url = `https://api.cloudflare.com/client/v4/zones/${zoneId}/dns_records?type=A&name=${name}`;
+
+  const response = await http.get(url, headers);
+  const body = await response.readBody();
+  const json = JSON.parse(body);
+  console.log(json);
+  if (json.success) {
+    const record = json.result.find((record: any) => record.name === name);
+    if (record) {
+      return record.id;
+    }
+  }
 }
